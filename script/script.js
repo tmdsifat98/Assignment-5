4db59cb412c53604dfdd899be275b158633a28d5
sifat tarafder <tmdsifat98@gmail.com>
2025-03-01 00:16:59 +0600

    responsive and all card added

--- script/script.js
+++ script/script.js
@@ -6,14 +6,14 @@ document
   .addEventListener("click", function (event) {
     rotation += 30;
     document.getElementById("rotate-theme").style.transform = `rotate(${rotation}deg)`;
-    indexNum++;
-    let themeChange = document.querySelectorAll(".bg-theme");
+    
+    let themeChange = document.querySelectorAll(".bg-gray-200");
     const colorPallet = [
       "bg-red-300",
       "bg-green-400",
       "bg-purple-300",
       "bg-blue-300",
-      "bg-gray-300",
+      "bg-violet-300",
       "bg-pink-300",
       "bg-orange-300",
       "bg-amber-300",
@@ -28,4 +28,5 @@ document
       }
       themeChange[i].classList.add(newColor);
     }
+    indexNum++;
   });
